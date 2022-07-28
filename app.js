a72e60f44a0a3cb4a569013a92bed15e60c09754
noel <noelati@gmail.com>
2022-07-27 23:25:23 -0400

    notas

--- app.js
+++ app.js
@@ -3,29 +3,29 @@ const btnAnterior = document.getElementById('btnAnterior');
 const btnSiguiente = document.getElementById('btnSiguiente');
 
 btnSiguiente.addEventListener('click', () => {
-	if(pagina < 1000){
+	if (pagina < 1000) {
 		pagina += 1;
 		cargarPeliculas();
 	}
 });
 
 btnAnterior.addEventListener('click', () => {
-	if(pagina > 1){
+	if (pagina > 1) {
 		pagina -= 1;
 		cargarPeliculas();
 	}
 });
 
-const cargarPeliculas = async() => {
+const cargarPeliculas = async () => {
 	try {
 		const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=${pagina}`);
-	
+
 		console.log(respuesta);
 
 		// Si la respuesta es correcta
-		if(respuesta.status === 200){
+		if (respuesta.status === 200) {
 			const datos = await respuesta.json();
-			
+
 			let peliculas = '';
 			datos.results.forEach(pelicula => {
 				peliculas += `
@@ -38,18 +38,27 @@ const cargarPeliculas = async() => {
 
 			document.getElementById('contenedor').innerHTML = peliculas;
 
-		} else if(respuesta.status === 401){
+		} else if (respuesta.status === 401) {
 			console.log('Pusiste la llave mal');
-		} else if(respuesta.status === 404){
+		} else if (respuesta.status === 404) {
 			console.log('La pelicula que buscas no existe');
 		} else {
 			console.log('Hubo un error y no sabemos que paso');
 		}
 
-	} catch(error){
+	} catch (error) {
 		console.log(error);
 	}
 
 }
 
 cargarPeliculas();
+
+/* 
+fetch es una función que nos permite colocar una cadena de texto, que es la dirección url a la que se quiere hacer 
+la petición
+
+Cuando se usa fetch nos vá a devolver una promesa, y esa promesa se está guardando en la variable respuesta.
+
+
+ */
